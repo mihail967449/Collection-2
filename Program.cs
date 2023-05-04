7fe732461263b633271776acb9f4ebb2056d411e
ИИП ТТ <shindanotamashi@gmail.com>
2023-05-04 12:54:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    /// <summary>
+    /// Коллекция женских имен
+    /// </summary>
+    class Program
+    {
+        static void Output(List<string>ints)
+        {
+            foreach (string element in ints)
+            {
+                Console.Write($"{element}\t");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var name = new List<string>(){"Anna,", "Ira,", "Nona"};
+            Output(name);
+           
+            Console.ReadLine();
+        }
+    }
+}
